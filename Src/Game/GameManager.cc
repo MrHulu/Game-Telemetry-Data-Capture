cdfdcedee2517c0a3f0d7a4418ccb766c76df763
Joker <491849417@qq.com>
2022-07-04 22:16:49 +0800

    feat: [Chinese] 添加游戏管理单例
    [English] Add GameManager Single instance

---
+++ Src/Game/GameManager.cc
@@ -0,0 +1,81 @@
+﻿#include "Game/GameManager.h"
+#include "Game/GameRunMonitoringHelper.h"
+#include "Game/Assetto Corsa/ACGame.h"
+#include "Game/Assetto Corsa Competizione/ACCGame.h"
+#include "Game/DiRT Rally 2.0/DirtRally2Game.h"
+#include "Game/DiRT 4/Dirt4Game.h"
+
+#include <algorithm>
+#include <windows.h>
+#include <tlhelp32.h>
+
+class 创建进程快照失败{};
+
+
+GameManager::GameManager(QObject *parent) : QObject(parent)
+{   
+    auto insertGame = [&](Game* game) {
+        m_supportGames.insert(game->name(), game);
+    };
+    insertGame(new ACGame(this));
+    insertGame(new ACCGame(this));
+    insertGame(new DirtRally2Game(this));
+    insertGame(new Dirt4Game(this));
+
+    m_gameRunMonitoringHelper = new GameRunMonitoringHelper(m_supportGames, this);
+    connect(m_gameRunMonitoringHelper, &GameRunMonitoringHelper::gameIsRunning, this, &GameManager::onGameIsRunning);
+}
+
+GameManager &GameManager::instance()
+{
+    static GameManager _instance;
+    return _instance;
+}
+
+void GameManager::setRunningGame(Game *newRunningGame)
+{
+    if (m_runningGame == newRunningGame)
+        return;
+    if ( m_runningGame != nullptr) {
+        m_runningGame->stopCapturer();
+        m_runningGame->disconnect(nullptr, this, nullptr); //TODO: 不应该删除所有信号
+    }
+
+    m_runningGame = newRunningGame;
+    emit runningGameChanged(m_runningGame);
+    controlGameCapturer();
+    if(m_runningGame == nullptr)
+        return;
+    connect(m_runningGame, &Game::isRunningChanged, this, &GameManager::controlGameCapturer);
+}
+
+Game *GameManager::findGame(const QString &name) const
+{
+    if (auto iter = m_supportGames.find(name);
+        iter != m_supportGames.end())
+    {
+        return m_supportGames.value(name);
+    }
+    return nullptr;
+}
+
+void GameManager::onGameIsRunning(Game *game)
+{
+    if(game && m_runningGame == nullptr)
+        setRunningGame(game);
+    else if(game == nullptr)
+        setRunningGame(nullptr);
+}
+
+void GameManager::controlGameCapturer()
+{
+    if(!m_runningGame)
+        return;
+    if(m_runningGame->isRunning())
+        m_runningGame->startCapturer();
+    else{
+        m_runningGame->stopCapturer();
+    }
+}
+
+
---
+++ Src/Game/GameManager.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QTimer>
+#include <QDateTime>
+#include <QMap>
+
+class Game;
+class GameRunMonitoringHelper;
+
+class GameManager : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(Game* runningGame READ runningGame WRITE setRunningGame NOTIFY runningGameChanged)
+    Q_PROPERTY(QMap<QString,Game*> supportGames READ supportGames CONSTANT)
+
+public:
+    explicit GameManager(QObject *parent = nullptr);
+    static GameManager& instance();
+
+    Game *runningGame() const;
+    void setRunningGame(Game *newRunningGame);
+    const QMap<QString, Game *> &supportGames() const;
+    Q_INVOKABLE Game* findGame(const QString& name) const;
+
+signals:
+    void runningGameChanged(Game* runningGame);
+
+private slots:
+    void onGameIsRunning(Game *game);
+    void controlGameCapturer();
+
+private:
+    GameRunMonitoringHelper*    m_gameRunMonitoringHelper = nullptr;
+    Game *m_runningGame = nullptr;
+    QMap<QString, Game *> m_supportGames;
+};
+
+inline Game *GameManager::runningGame() const { return m_runningGame; }
+inline const QMap<QString, Game *> &GameManager::supportGames() const { return m_supportGames; }
