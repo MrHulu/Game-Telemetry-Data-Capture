21f979709434aaa1cc9d233077f14ba06cc3d246
Joker <491849417@qq.com>
2022-07-05 23:36:57 +0800

    feat: Add Game/GameInfoHelperFileAnalysis

---
+++ Src/Game/GameInfoHelperFileAnalysis.cc
@@ -0,0 +1,22 @@
+﻿#include "Game/GameInfoHelperFileAnalysis.h"
+
+namespace Envirnment {
+    extern QString applicationInfoPath();
+}// Envirnment
+
+GameInfoHelper::GameInfoFileAnalysis::GameInfoFileAnalysis(GameInfoHelper& helper)
+    :   QObject{&helper},
+        gameInfoHelper{helper}
+{
+
+}
+
+void GameInfoHelper::GameInfoFileAnalysis::analyze()
+{
+
+}
+
+QString GameInfoHelper::GameInfoFileAnalysis::gameInfoFilePath()
+{
+    return QString("%1/%2").arg(applicationInfoPath()).arg(GameInfoFileName);
+}
---
+++ Src/Game/GameInfoHelperFileAnalysis.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Game/GameInfoHelper.h"
+
+#include <QObject>
+#include <QDateTime>
+
+class GameInfoHelper::GameInfoFileAnalysis : public QObject
+{
+    Q_OBJECT
+    static constexpr auto GameInfoFileName = "GameInfo.json";
+public:
+    GameInfoFileAnalysis(GameInfoHelper& helper);
+
+    void analyze();
+
+private:
+    QString gameInfoFilePath();
+
+
+private:
+    GameInfoHelper& gameInfoHelper;
+
+    QByteArray      m_fileData;
+    QString         m_fileVersion;
+    QDateTime       m_fileChangeTime;
+};
+
