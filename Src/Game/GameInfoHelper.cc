33c9bd2d6365b7720772f857e2cabe714a4be651
Joker <491849417@qq.com>
2022-07-05 23:36:38 +0800

    feat: Add GameInfoHelper

---
+++ Src/Game/GameInfoHelper.cc
@@ -0,0 +1,14 @@
+﻿#include "Game/GameInfoHelper.h"
+#include "Game/Game.h"
+
+GameInfoHelper::GameInfoHelper(const QMap<QString, Game*>& games, QObject *parent)
+    :   QObject{parent},
+        m_games{games}
+{
+
+}
+
+void GameInfoHelper::updateGameInfo()
+{
+
+}
---
+++ Src/Game/GameInfoHelper.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <QObject>
+#include <QMap>
+
+class Game;
+
+class GameInfoHelper : public QObject
+{
+    Q_OBJECT
+public:
+    GameInfoHelper(const QMap<QString, Game*>& games, QObject *parent = nullptr);
+
+    void updateGameInfo();
+
+private:
+    QMap<QString, Game*> m_games;
+
+    class GameInfoFileAnalysis;
+    GameInfoFileAnalysis*   m_gameInfoFileAnalysis = nullptr;
+
+};
+
