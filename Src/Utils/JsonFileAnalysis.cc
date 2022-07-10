5a3af31275cb786f803c8eb3a75666f8227b5285
Joker <491849417@qq.com>
2022-07-10 20:54:33 +0800

    feat: Add Json file analyze class

---
+++ Src/Utils/JsonFileAnalysis.cc
@@ -0,0 +1,50 @@
+﻿#include "Utils/JsonFileAnalysis.h"
+
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonParseError>
+#include <QFile>
+#include <QDebug>
+
+JsonFileAnalysis::JsonFileAnalysis(QString filePath, QObject *parent)
+    :   QObject{parent},
+        m_filePath{filePath}
+{
+    analyze();
+}
+
+void JsonFileAnalysis::analyze()
+{
+    QFile file(m_filePath);
+    if(file.open(QIODevice::ReadOnly)) {
+        m_fileData = file.readAll();
+        file.close();
+    }
+    else
+        qWarning() << QString("[JsonFileAnalysis::analyze()]： 文件%1无法打开！%2").arg(filePath(), file.errorString());
+}
+
+QJsonArray JsonFileAnalysis::toJsonArray()
+{
+    QJsonArray arr;
+    QJsonParseError err;
+    auto jsonDoc = QJsonDocument::fromJson(m_fileData, &err);
+    if(err.error == QJsonParseError::NoError)
+        arr = jsonDoc.array();
+    else
+        qWarning() << QString("[JsonFileAnalysis::toJsonArray()]: 文件%1不是Json格式！%2").arg(filePath(), err.errorString());
+    return arr;
+}
+
+QJsonObject JsonFileAnalysis::toJsonObjcet()
+{
+    QJsonObject obj;
+    QJsonParseError err;
+    auto jsonDoc = QJsonDocument::fromJson(m_fileData, &err);
+    if(err.error == QJsonParseError::NoError)
+        obj = jsonDoc.object();
+    else
+        qWarning() << QString("[JsonFileAnalysis::toJsonObjcet()]: 文件%1不是Json格式！%2").arg(filePath(), err.errorString());
+    return obj;
+
+}
---
+++ Src/Utils/JsonFileAnalysis.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <QObject>
+
+class QJsonObject;
+class QJsonArray;
+
+class JsonFileAnalysis : public QObject
+{
+public:
+    JsonFileAnalysis(QString filePath, QObject* parent = nullptr);
+
+    void analyze();
+    QString filePath() const;
+    QByteArray toByteArray();
+    QJsonArray toJsonArray();
+    QJsonObject toJsonObjcet();
+
+private:
+    QString     m_filePath;
+    QByteArray  m_fileData;
+};
+
+inline QString JsonFileAnalysis::filePath() const { return m_filePath; }
+
+inline QByteArray JsonFileAnalysis::toByteArray() { return m_fileData; }
+
