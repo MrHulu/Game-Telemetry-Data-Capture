4eae35bf6562bb29db2f894a890ee1f52e8972aa
Joker <491849417@qq.com>
2022-07-14 22:48:45 +0800

    test(GameInfoHelper): 搭建单元测试

---
+++ Tests/GameInfoReadWrite/tst_GameInfoReadWrite.cc
@@ -0,0 +1,40 @@
+﻿#include <QtTest>
+#include <QSignalSpy>
+
+// add necessary includes here
+#include "Game/GameInfoHelper.h"
+
+#include <memory>
+
+class tst_GameInfoReadWrite : public QObject
+{
+    Q_OBJECT
+public:
+    tst_GameInfoReadWrite();
+    ~tst_GameInfoReadWrite();
+
+private slots:
+
+private:
+};
+
+tst_GameInfoReadWrite::tst_GameInfoReadWrite()
+{
+
+
+}
+
+tst_GameInfoReadWrite::~tst_GameInfoReadWrite()
+{
+
+}
+
+
+
+
+
+
+
+QTEST_MAIN(tst_GameInfoReadWrite)
+
+#include "tst_GameInfoReadWrite.moc"
