3241410f2387d5cdff6f9bee4dcb155e4a549eb5
Joker <491849417@qq.com>
2022-07-14 22:44:13 +0800

    test(GameRunMonitoringHelper): 添加单元测试并通过测试

--- Tests/GameRunMonitoring/tst_GameRunMonitoring.cc
+++ Tests/GameRunMonitoring/tst_GameRunMonitoring.cc
@@ -1,10 +1,12 @@
 ﻿#include <QtTest>
+#include <QSignalSpy>
 
 // add necessary includes here
+#include "Game/Assetto Corsa/ACGame.h"
+#include "Game/GameRunMonitoringHelper.h"
 #include "Game/TelemetryDataBuffer.h"
 
 #include <memory>
-
 
 class tst_GameRunMonitoring : public QObject
 {
@@ -13,21 +15,53 @@ public:
     tst_GameRunMonitoring();
     ~tst_GameRunMonitoring();
 
-
 private slots:
+    void tst_MonitoringNoRunning();
+    void tst_MonitoringRunning();
 
 private:
-
+    ACGame* ac;
+    GameRunMonitoringHelper* helper;
+    Game* currentRunGame;
+    bool isReceiveGameRunSignal = false;
 };
 
-tst_GameRunMonitoring::tst_GameRunMonitoring()
+tst_GameRunMonitoring::tst_GameRunMonitoring() : ac(new ACGame(this))
 {
+    helper = new GameRunMonitoringHelper(QMap<QString, Game*>{ {ac->name(), ac} });
+    connect(helper, &GameRunMonitoringHelper::gameIsRunning, this, [this](Game* game) {
+        currentRunGame = game;
+        isReceiveGameRunSignal = true;
+    });
 
 }
 
 tst_GameRunMonitoring::~tst_GameRunMonitoring()
 {
 
+}
+
+void tst_GameRunMonitoring::tst_MonitoringNoRunning()
+{
+
+    isReceiveGameRunSignal = false;
+    QSignalSpy spy(helper, &GameRunMonitoringHelper::gameIsRunning);
+    if(!spy.wait(2000))
+        QFAIL("无法启动监控!");
+    QVERIFY(isReceiveGameRunSignal);
+    QCOMPARE(currentRunGame, nullptr);
+
+}
+
+void tst_GameRunMonitoring::tst_MonitoringRunning()
+{
+    isReceiveGameRunSignal = false;
+    QSignalSpy spy(helper, &GameRunMonitoringHelper::gameIsRunning);
+    ac->setProcessNames(QSet<QString>{ "tst_GameRunMonitoring.exe" });
+    if(!spy.wait(2000))
+        QFAIL("无法启动监控!");
+    QVERIFY(isReceiveGameRunSignal);
+    QCOMPARE(currentRunGame, ac);
 }
 
 
