9f0dd9bc412e4d9ee22131188eadd60defe54adc
Joker <491849417@qq.com>
2022-07-14 22:06:16 +0800

    build: 搭建单元测试框架

---
+++ Tests/GameRunMonitoring/tst_GameRunMonitoring.cc
@@ -0,0 +1,39 @@
+﻿#include <QtTest>
+
+// add necessary includes here
+#include "Game/TelemetryDataBuffer.h"
+
+#include <memory>
+
+
+class tst_GameRunMonitoring : public QObject
+{
+    Q_OBJECT
+public:
+    tst_GameRunMonitoring();
+    ~tst_GameRunMonitoring();
+
+
+private slots:
+
+private:
+
+};
+
+tst_GameRunMonitoring::tst_GameRunMonitoring()
+{
+
+}
+
+tst_GameRunMonitoring::~tst_GameRunMonitoring()
+{
+
+}
+
+
+
+
+
+QTEST_MAIN(tst_GameRunMonitoring)
+
+#include "tst_GameRunMonitoring.moc"
