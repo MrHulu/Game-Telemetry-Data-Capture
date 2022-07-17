9ef70e3843699a63ec9ad55157b93df1dcfa9968
Joker <491849417@qq.com>
2022-07-17 16:51:57 +0800

    test(GameInfoHelper): 通过单元测试

--- Tests/GameInfoReadWrite/tst_GameInfoReadWrite.cc
+++ Tests/GameInfoReadWrite/tst_GameInfoReadWrite.cc
@@ -1,11 +1,11 @@
 ﻿#include <QtTest>
 #include <QSignalSpy>
+#include <QMap>
+#include <memory>
 
 // add necessary includes here
 #include "Game/GameInfoHelper.h"
 #include "Game/DiRT 4/Dirt4Game.h"
-
-#include <memory>
 
 class tst_GameInfoReadWrite : public QObject
 {
@@ -14,26 +14,51 @@ public:
     tst_GameInfoReadWrite();
 
 private slots:
-    void checkEmbeddeGameInfoFile();
-    void createLocalGameInfoFile();
+    void checkReadGameInfoFile();
+    void checkWriteGameInfoFile();
 
 private:
     Dirt4Game*  dirt4{};
+    GameInfoHelper* helper{};
 };
 
 tst_GameInfoReadWrite::tst_GameInfoReadWrite()
 {
     dirt4 = new Dirt4Game(this);
+    helper = new GameInfoHelper(QMap<QString, Game*>{ {dirt4->name(), dirt4} });
 }
 
-void tst_GameInfoReadWrite::checkEmbeddeGameInfoFile()
-{
 
+void tst_GameInfoReadWrite::checkReadGameInfoFile()
+{
+    QCOMPARE(dirt4->readPort(), 21466);
+    QCOMPARE(dirt4->writePort(), 21400);
+    QCOMPARE(dirt4->installPath(), nullptr);
+    QCOMPARE(dirt4->processNames(), QSet<QString>{"dirt4.exe"});
+
+    QSignalSpy spy(dirt4, &Dirt4Game::readPortChanged);
+    spy.wait(500);
+    helper->updateGameInfo();
+    QCOMPARE(dirt4->readPort(), 20777);
 }
 
-void tst_GameInfoReadWrite::createLocalGameInfoFile()
+void tst_GameInfoReadWrite::checkWriteGameInfoFile()
 {
+    QCOMPARE(dirt4->installPath(), nullptr);
 
+    // 修改 installPath 为 C:\\test\\dirt4.exe
+    QSignalSpy spy(dirt4, &Dirt4Game::installPathChanged);
+    spy.wait(200);
+    helper->writeGameInstallPathInfo(dirt4->name(), "C:\\test\\dirt4.exe");
+    helper->updateGameInfo();
+    QCOMPARE(dirt4->installPath(), "C:\\test\\dirt4.exe");
+
+    // 恢复
+    spy.clear();
+    helper->writeGameInstallPathInfo(dirt4->name(), nullptr);
+    helper->updateGameInfo();
+    spy.wait(200);
+    QCOMPARE(dirt4->installPath(), nullptr);
 }
 
 
